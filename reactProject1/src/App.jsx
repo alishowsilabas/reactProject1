d70cdccc48c8b57359395b667e62abcfc6050952
alishowsilabas <alissonsilabas@gmail.com>
2023-10-23 10:15:26 -0300

    Lista heroi com submit para usuario adicionar info

--- reactProject1/src/App.jsx
+++ reactProject1/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import classes from "./App.module.css";
+import styles from "styled-components";
 
 const App = () => {
   const heroisIniciais = [
@@ -9,19 +9,63 @@ const App = () => {
   ];
 
   const [herois, setHerois] = useState(heroisIniciais);
+  const [novoHeroi, setNovoHeroi] = useState({
+    nome: "",
+    heroi: "",
+  });
+
+  const handleChange = (event) => {
+    setNovoHeroi({
+      ...novoHeroi,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setHerois([...herois, novoHeroi]);
+    setNovoHeroi({
+      nome: "",
+      heroi: "",
+    });
+  };
 
   return (
-    <div>
-      {herois.map((item) => {
-        return (
-          <div className={classes.mainText}>
-            <h1>{item.heroi}</h1>
-            <h2>{item.nome}</h2>
-          </div>
-        );
-      })}
-    </div>
+    <Div>
+      <div>
+        {herois.map((item) => {
+          return (
+            <div key={item.nome}>
+              <h1>{item.heroi}</h1>
+              <h2>{item.nome}</h2>
+            </div>
+          );
+        })}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="Heroi"
+          value={novoHeroi.heroi}
+          name="heroi"
+        />
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="Nome"
+          value={novoHeroi.nome}
+          name="nome"
+        />
+        <button>Incluir Novo Heroi</button>
+      </form>
+      <div></div>
+    </Div>
   );
 };
+
+const Div = styles.div`
+  text-align: center;
+`;
 
 export default App;
